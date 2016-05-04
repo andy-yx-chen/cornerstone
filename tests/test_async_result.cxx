e0521d89065cc14d9a337b30d3e07bccbebfc10a
Andy Chen <andy@anchelab.guest.corp.microsoft.com>
2016-05-04 11:32:25 -0800

    Fixes for clang and pmake

--- tests/test_async_result.cxx
+++ tests/test_async_result.cxx
@@ -6,6 +6,8 @@
 
 using namespace cornerstone;
 
+typedef async_result<int>::handler_type int_handler;
+
 async_result<int>* create_and_set_async_result(int time_to_sleep, int value, std::exception* err) {
     async_result<int>* result = new async_result<int>();
     if (time_to_sleep <= 0) {
@@ -27,22 +29,25 @@ void test_async_result() {
         std::unique_ptr<async_result<int>> ptr(create_and_set_async_result(0, 123, nullptr));
         assert(123 == ptr->get());
         bool handler_called = false;
-        ptr->when_ready((async_result<int>::handler_type)([&handler_called](int val, std::exception* e) -> void {
+	int_handler h = (async_result<int>::handler_type)([&handler_called](int val, std::exception* e) -> void {
             assert(123 == val);
             assert(nullptr == e);
             handler_called = true;
-        }));
+	  });
+	
+        ptr->when_ready(h);
         assert(handler_called);
     }
 
     {
         std::unique_ptr<async_result<int>> ptr(create_and_set_async_result(200, 496, nullptr));
         bool handler_called = false;
-        ptr->when_ready((async_result<int>::handler_type)([&handler_called](int val, std::exception* e) -> void {
+	int_handler h = (async_result<int>::handler_type)([&handler_called](int val, std::exception* e) -> void {
             assert(496 == val);
             assert(nullptr == e);
             handler_called = true;
-        }));
+	  });
+        ptr->when_ready(h);
         assert(496 == ptr->get());
         std::this_thread::sleep_for(std::chrono::milliseconds(100));
         assert(handler_called);
@@ -57,10 +62,11 @@ void test_async_result() {
         std::exception* ex = new std::bad_exception();
         std::unique_ptr<async_result<int>> ptr(create_and_set_async_result(200, 496, ex));
         bool handler_called = false;
-        ptr->when_ready((async_result<int>::handler_type)([&handler_called,ex](int val, std::exception* e) -> void {
+	int_handler h = (async_result<int>::handler_type)([&handler_called,ex](int val, std::exception* e) -> void {
             assert(ex == e);
             handler_called = true;
-        }));
+	  });
+        ptr->when_ready(h);
 
         bool ex_handled = false;
         try {
